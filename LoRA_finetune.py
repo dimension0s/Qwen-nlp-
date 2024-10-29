b0daa3558d51ada613eeb0afc92c7d3949db174b
bina <149747333+dimension0s@users.noreply.github.com>
2024-10-29 16:28:25 +0800

    Update LoRA_finetune.py

--- LoRA_finetune.py
+++ LoRA_finetune.py
@@ -20,6 +20,9 @@ lora_config = LoraConfig(
     lora_dropout=0.1  # dropout比例
 )
 model = get_peft_model(model, lora_config)
-
-
+# 如果出现问题：使用的 CUDA 版本或 PyTorch 不支持在 BFloat16 下执行某些操作：
+# 将模型的输入和参数转换为 Float16 或 Float32
+# model.half()  # 将模型转换为 float16
+# 或者：使用 float32
+# model.float()
 
