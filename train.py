10dfcd10439b2fd68c6ff8588bea0f56f8519cbd
bina <149747333+dimension0s@users.noreply.github.com>
2024-10-29 16:26:09 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 from tqdm.auto import tqdm
+from torch.cuda.amp import autocast, GradScaler
+# 混合精度训练（提高计算效率和内存利用率）
+scaler = GradScaler()
 
 def train_loop(dataloader, model, optimizer, lr_scheduler, epoch, total_loss):
     model.train()
@@ -9,15 +12,21 @@ def train_loop(dataloader, model, optimizer, lr_scheduler, epoch, total_loss):
         model_inputs = batch_data['input_ids']
         attention_mask = batch_data['attention_mask']
         labels = batch_data['labels']
-        outputs = model(
-            input_ids=model_inputs,
-            attention_mask=attention_mask,
-            labels=labels)
-        loss = outputs.loss
+        with autocast():         
+            outputs = model(
+                input_ids=model_inputs,
+                attention_mask=attention_mask,
+                labels=labels)
+            loss = outputs.loss
+        
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()  # 缩放损失以避免 FP16 下的数值溢出
+        scaler.step(optimizer)  # 更新权重
+        scaler.update()  # 更新缩放因子
+        # 不做混合精度训练：
+        # loss.backward()
+        # optimizer.step()
         lr_scheduler.step()
 
         total_loss += loss.item()
